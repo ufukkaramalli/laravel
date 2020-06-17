624909422c9f1d111d9458676fe3c3e446368d92
ufukkaramalli <ufukkaramalli@gmail.com>
2020-06-18 00:31:58 +0300

    laravel/passport added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link 
         <!-- Styles -->
         <style>
             html, body {
@@ -63,7 +63,7 @@
             }
         </style>
     </head>
-    <body>
+    <body >
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -79,7 +79,8 @@
                 </div>
             @endif
 
-            <div class="content">
+            <div  class="content">
+            
                 <div class="title m-b-md">
                     Laravel
                 </div>
@@ -92,6 +93,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <script src='js/app.js'></script> 
                 </div>
             </div>
         </div>
